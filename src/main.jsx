6e20654f462545f3b134ece5e0f04cd5f03e228c
Mario Borges <popesco19@hotmail.com>
2024-10-11 18:07:53 +0100

    Refactor import statement for Home component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/Home'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
