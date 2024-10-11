c76d663c6fcb07690259a82ea30204bf35706739
Mario Borges <popesco19@hotmail.com>
2024-10-11 20:03:10 +0100

    Refactor api.js to add token authentication interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,18 @@ const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/',
 });
 
+// Interceptor to add the token in the header of the request
+api.interceptors.request.use(
+  config => {
+    const authTokens = localStorage.getItem('authTokens')
+      ? JSON.parse(localStorage.getItem('authTokens'))
+      : null;
+    if (authTokens && authTokens.access) {
+      config.headers.Authorization = `Bearer ${authTokens.access}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
 export default api;
