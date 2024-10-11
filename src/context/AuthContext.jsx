e3919d0af75656b288e61f787db4c0cb0cb0a8eb
Mario Borges <popesco19@hotmail.com>
2024-10-11 20:10:05 +0100

    Refactor AuthContext to use jwt-decode for decoding access token

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,38 +1,42 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { loginUser, refreshAccessToken } from '../services/auth';
-
+import jwt_decode from 'jwt-decode';
+import { loginUser } from '../services/auth';
 
 const AuthContext = createContext();
 
-
 export const AuthProvider = ({ children }) => {
-    const [authTokens, setAuthTokens] = useState(() =>
-      localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
-    );
-    const [user, setUser] = useState(() =>
-      localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')).user : null
-    );
+  const [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
+  );
+  const [user, setUser] = useState(() =>
+    localStorage.getItem('authTokens') ? jwt_decode(JSON.parse(localStorage.getItem('authTokens')).access) : null
+  );
 
+  useEffect(() => {
+  }, [authTokens, user]);
 
-    const login = async (credentials) => {
-        const data = await loginUser(credentials);
-        setAuthTokens(data);
-        setUser(data.user);
-        localStorage.setItem('authTokens', JSON.stringify(data));
-      };
-    
+  const login = async (credentials) => {
+    try {
+      const data = await loginUser(credentials);
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem('authTokens', JSON.stringify(data));
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  };
 
-      const logout = () => {
-        setAuthTokens(null);
-        setUser(null);
-        localStorage.removeItem('authTokens');
-      };
-    
-      return (
-        <AuthContext.Provider value={{ authTokens, user, login, logout }}>
-          {children}
-        </AuthContext.Provider>
-      );
-    };
-    
-    export default AuthContext;
+  const logout = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem('authTokens');
+  };
+
+  return (
+    <AuthContext.Provider value={{ authTokens, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
