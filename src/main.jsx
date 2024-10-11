b6755b1325b63077ac0885920d4e08f3386d981c
Mario Borges <popesco19@hotmail.com>
2024-10-11 18:41:02 +0100

    Refactor Home component import to use HomeWrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/home'
+import HomeWrapper from './pages/home';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <HomeWrapper />
   </StrictMode>,
 )
