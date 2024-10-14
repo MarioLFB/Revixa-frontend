87b491bedb2d57941c231f11209ebdd4b5451b88
Mario Borges <popesco19@hotmail.com>
2024-10-14 17:28:44 +0100

    Add to postService.js createPost and updatePost

--- src/services/postService.js
+++ src/services/postService.js
@@ -11,3 +11,20 @@ export const getPosts = async (token) => {
   return response.data;
 };
 
+export const createPost = async (postData, token) => {
+  const response = await axios.post(API_URL, postData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const updatePost = async (postId, postData, token) => {
+  const response = await axios.put(`${API_URL}${postId}/`, postData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
