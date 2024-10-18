f42b9ed27cba44be449aed4c82ddf68b1c9c0986
Mario Borges <popesco19@hotmail.com>
2024-10-18 18:34:12 +0100

    Add likePost and unlikePost functions to handle post liking and unliking

---
+++ src/services/like.js
@@ -0,0 +1,21 @@
+import api from './api';
+
+export async function likePost(postId) {
+    try {
+        const response = await api.get(`/social/posts/${postId}/like/`);
+        return response.data;
+    } catch (error) {
+        console.error('Error when liking post:', error);
+        throw error;
+    }
+}
+
+export async function unlikePost(postId) {
+    try {
+        const response = await api.delete(`/social/posts/${postId}/like/`);
+        return response.data;
+    } catch (error) {
+        console.error('Error when unliking post:', error);
+        throw error;
+    }
+}
