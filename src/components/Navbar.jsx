0e8dce0a985c5e9420ae4dc868c4ef46553ecd12
Mario Borges <popesco19@hotmail.com>
2024-10-21 18:43:49 +0100

    Refactor Navbar component to use react-router-dom for navigation and add login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function MyNavbar() {
   return (
     <Container>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#">Revixa</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className="navbar-brand">Revixa</Link>
+          </Navbar.Brand>
+          <div className="ml-auto">
+            <Link to="/login" className="btn btn-primary">
+              Login
+            </Link>
+          </div>
         </Container>
       </Navbar>
     </Container>
