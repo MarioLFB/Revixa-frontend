06d484bc4e4b3836b524c6b602febfd395a85c67
Mario Borges <popesco19@hotmail.com>
2024-10-21 19:34:08 +0100

    Refactor Navbar component to include user authentication and logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
 function MyNavbar() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <Container>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -11,9 +15,18 @@ function MyNavbar() {
             <Link to="/" className="navbar-brand">Revixa</Link>
           </Navbar.Brand>
           <div className="ml-auto">
-            <Link to="/login" className="btn btn-primary">
-              Login
-            </Link>
+            {user ? (
+              <>
+                <span className="navbar-text me-3">Hello, {user.username}</span>
+                <button className="btn btn-outline-danger" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <Link to="/login" className="btn btn-primary">
+                Login
+              </Link>
+            )}
           </div>
         </Container>
       </Navbar>
