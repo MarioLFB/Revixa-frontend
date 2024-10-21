43928752a0546da1e41604a70acb993a623db62a
Mario Borges <popesco19@hotmail.com>
2024-10-21 19:04:03 +0100

    Removes login system from the Home page to implement in the navigation bar. Add Bootstrap style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,20 @@
 import React, { useContext } from 'react';
 import AuthContext from '../../context/AuthContext';
-import Login from '../Login';
 import Reviews from '../Reviews/Reviews';
 
 function Home() {
-  const { user, logout } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
-    <div>
+    <div className="container mt-4">
       <h1>Revixa Home Page</h1>
       {user ? (
         <div>
           <h2>Welcome, {user.username}!</h2>
-          <button onClick={logout}>Logout</button>
           <Reviews />
         </div>
       ) : (
-        <Login />
+        <h2>Please log in to access the content.</h2>
       )}
     </div>
   );
