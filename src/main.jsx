2591060c52fb74a49bd9a9e503579d52baedcc1a
Mario Borges <popesco19@hotmail.com>
2024-10-21 17:50:06 +0100

    Update package-lock.json, package.json and main.jsx to include Bootstrap and Popper.js dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HomeWrapper from "./pages/home";
 import { AuthProvider } from "./context/AuthContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
