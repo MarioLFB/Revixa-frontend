b40bc4ffdfb41f7b51050d0a4cb415e2f3d61a58
Mario Borges <popesco19@hotmail.com>
2024-10-22 19:12:09 +0100

    Add Dashboard page and add new route to dashboard

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Reviews from "../Reviews/Reviews";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Reviews />
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import Login from '../Login';
 import Reviews from '../Reviews/Reviews';
 import ReviewPosts from '../Reviews/ReviewPosts';
 import MyNavbar from '../../components/Navbar';
+import Dashboard from '../Dashboard';
 
 
 
@@ -17,6 +18,7 @@ function HomeWrapper() {
         <Route path="/login" element={<Login />} /> {/* Login Page */}
         <Route path="/reviews" element={<Reviews />} /> {/* Reviews Page */}
         <Route path="/reviews/:reviewId/posts" element={<ReviewPosts />} /> {/* Show Posts in Reviews Page */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Page */}
       </Routes>
     </Router>
   );
