879da2d3e14adb7af2b5ee7bf7cb82abb8d9b46f
Mario Borges <popesco19@hotmail.com>
2024-10-23 19:55:44 +0100

    Remove styled components and DashboardWrapper

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Reviews from "../Reviews/Reviews";
-import styled from "styled-components";
 
-const DashboardWrapper = styled.div`
-  height: 100vh;
-  background: linear-gradient(90deg, #FF7F50, #FFA500);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 function Dashboard() {
   return (
-    <DashboardWrapper>
       <div>
         <Reviews />
       </div>
-    </DashboardWrapper>
   );
 }
 
