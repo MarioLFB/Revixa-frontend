1a726f0ca93d640c9efa406fb3376f543f68c9ad
Mario Borges <popesco19@hotmail.com>
2024-10-23 19:19:54 +0100

    add styled DashboardWrapper with orange gradient background and centered layout

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Reviews from "../Reviews/Reviews";
+import styled from "styled-components";
+
+const DashboardWrapper = styled.div`
+  height: 100vh;
+  background: linear-gradient(90deg, #FF7F50, #FFA500); /* Gradiente mais alaranjado */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Reviews />
-    </div>
+    <DashboardWrapper>
+      <div>
+        <h1>Dashboard</h1>
+        <Reviews />
+      </div>
+    </DashboardWrapper>
   );
 }
 
