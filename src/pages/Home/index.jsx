283182d48595ba7592ab8a08cab0733a429f3400
Mario Borges <popesco19@hotmail.com>
2024-10-25 18:34:35 +0100

    Add Footer component to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ import Reviews from '../Reviews/Reviews';
 import ReviewPosts from '../Reviews/ReviewPosts';
 import MyNavbar from '../../components/Navbar';
 import Dashboard from '../Dashboard';
+import Footer from '../../components/Footer';
 
 
 
@@ -21,6 +22,7 @@ function HomeWrapper() {
         <Route path="/reviews/:reviewId/posts" element={<ReviewPosts />} /> {/* Show Posts in Reviews Page */}
         <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Page */}
       </Routes>
+      <Footer /> {/* Footer */}
     </Router>
   );
 }
